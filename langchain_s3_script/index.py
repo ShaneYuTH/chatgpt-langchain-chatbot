8202afca6a604db27b03294c6b037d4890523ac2
Tianhao Yu <58100540+ShaneYuTH@users.noreply.github.com>
2023-07-05 00:17:05 -0700

    Add files via upload

--- langchain_s3_script/index.py
+++ langchain_s3_script/index.py
@@ -15,17 +15,21 @@ from langchain.chains import ConversationalRetrievalChain
 
 import hr_stuff_prompt
 
+# Initialize S3 and SQS clients
 s3 = boto3.client('s3')
 sqs = boto3.client('sqs')
 
+# Get queue URL from environment variables
 queue_url = os.getenv('SQS_QUEUE_URL')
 
+# Function to delete a file from given path and handle any OS error
 def delete_file(path):
     try:
         os.remove(path)
     except OSError as e:
         print(f"Error: {e.filename} - {e.strerror}.")
 
+# Function to process a file: download it, process it, and then upload it back to S3
 def process_file(bucket, key):
     download_path = '/tmp/{}'.format(os.path.basename(key))
     try:
@@ -34,6 +38,7 @@ def process_file(bucket, key):
         print(f"Error downloading file: {e}")
         return
 
+    # Load JSON content using a specific schema
     loader = JSONLoader(
         file_path=download_path,
         jq_schema='.hits.hits[]._source',
@@ -48,7 +53,9 @@ def process_file(bucket, key):
         else:
             raise e
 
+    # Build a vector store (FAISS) using OpenAI embeddings
     db = FAISS.from_documents(docs, OpenAIEmbeddings())
+    # Create a conversation memory buffer
     memory = ConversationBufferMemory(
         memory_key="chat_history", return_messages=True)
 
@@ -56,6 +63,7 @@ def process_file(bucket, key):
         ChatOpenAI(temperature=0, model="gpt-4"))
     CONDENSE_QUESTION_PROMPT = hr_stuff_prompt.CONDENSE_QUESTION_PROMPT
 
+    # Set up the conversational retrieval chain
     qa = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(temperature=0, model="gpt-4"),
         retriever=db.as_retriever(search_kwargs={"k": 10}),
@@ -65,9 +73,10 @@ def process_file(bucket, key):
         combine_docs_chain_kwargs={"prompt": PROMPT},
         memory=memory,)
 
+    # Save the conversational retrieval chain (qa) to a local pickle file
     with open('/tmp/qa.pkl', 'wb') as f:
         pickle.dump(qa, f)
-
+    # Upload the pickle file to the same S3 bucket
     try:
         s3.upload_file('/tmp/qa.pkl', bucket, key.replace('.json', '.pkl'))
         print("File successfully uploaded")
@@ -76,9 +85,11 @@ def process_file(bucket, key):
     except Exception as e:
         print(f"Error uploading file: {e}")
     finally:
+        # Clean up local files
         delete_file('/tmp/qa.pkl')
         delete_file(download_path)
 
+# Function to handle incoming SQS messages and process corresponding S3 files
 def handler():    
     while True:
         response = sqs.receive_message(
@@ -108,4 +119,5 @@ def handler():
         else:
             print("No new files found")
 
+# Start the handler
 handler()
